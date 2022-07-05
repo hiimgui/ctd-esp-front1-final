829808571e693d881395c089528d583421be5e0b
GuilhermeKageyama <guiproc@gmail.com>
2022-07-04 22:52:56 -0300

    actions

---
+++ src/store/actions/characters.actions.tsx
@@ -0,0 +1,29 @@
+//4 actions principais
+type ICharacter = {
+  id: number;
+  name: string;
+};
+export function fetchCharactersStart() {
+  return {
+    type: "FETCH_CHARACTERS_START",
+  };
+}
+export function filterCharactersStart(searchText: string) {
+  return {
+    type: "FILTER_CHARACTERS_START",
+    payload: searchText,
+  };
+}
+
+export function fetchCharactersSuccess(characters: ICharacter[]) {
+  return {
+    type: "FETCH_CHARACTERS_START",
+    payload: characters,
+  };
+}
+export function fetchCharactersError(error: string) {
+  return {
+    type: "FETCH_CHARACTERS_ERROR",
+    payload: error,
+  };
+}
