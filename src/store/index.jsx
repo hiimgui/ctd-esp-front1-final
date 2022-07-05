7275a1e4b84532e82a625914535c3d82060da7b6
GuilhermeKageyama <guiproc@gmail.com>
2022-07-05 10:00:23 -0300

    sagas + debug de render no index.jsx

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-import reducers from "../reducers";
-import sagas from "../sagas";
+import reducers from "../store/reducers/index";
+import sagas from "../store/sagas";
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 export default store;
---
+++ src/store/sagas/index.tsx
@@ -0,0 +1,38 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+import { fetchCharactersSuccess, fetchCharactersError } from "../actions/characters.actions";
+
+function* fetchCharacterSaga() {
+  try {
+    //@ts-ignore
+    const response = yield call(fetch, "https://rickandmortyapi.com/api/character");
+    //@ts-ignore
+    const data = yield response.json();
+    yield put(fetchCharactersSuccess(data));
+  } catch (error : any) {
+    yield put(fetchCharactersError(error.message));
+  }
+}
+
+//responsavel por funcao de filtro
+//@ts-ignore
+function* filterCharactersSaga({ payload }) {
+  try {
+    //@ts-ignore
+    const response = yield call(
+      fetch,
+      `https://rickandmortyapi.com/api/character/${payload}`
+    );
+    //@ts-ignore
+    const data = yield response.json();
+    yield put(fetchCharactersSuccess(data));
+  } catch (error) {
+    //@ts-ignore
+    yield put(fetchCharactersError(error.message));
+  }
+}
+
+export default function* sagas() {
+  yield takeLatest("FETCH_CHARACTERS_START", fetchCharacterSaga);
+  //@ts-ignore
+  yield takeLatest("FILTER_CHARACTERS_START", filterCharactersSaga);
+}
